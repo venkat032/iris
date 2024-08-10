310af87d0627eb4f4dbe7c9bb7a556c5d2ea07ec
venkat032 <136120829+venkat032@users.noreply.github.com>
2024-08-10 23:02:46 +0530

    Add files via upload

---
+++ deploy.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import pickle
+import joblib
+
+
+app = Flask(__name__,template_folder="templates")
+#load the model
+model =joblib.load(open('save_model.sav', 'rb'))
+
+@app.route('/')
+def home():
+    result = ''
+    return render_template('index.html',**locals())
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    sepal_length=float(request.form['sepal_length'])
+    sepal_width=float(request.form['sepal_width'])
+    petal_length=float(request.form['petal_length'])
+    petal_width=float(request.form['petal_width'])
+    result = model.predict([[sepal_length,sepal_width,petal_length,petal_width]])
+    #returning html page
+    return render_template('index.html',**locals())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
